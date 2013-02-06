6941f674bcb42f8641b70909792e82dd58038dce
ivany4 <ivany4@me.com>
2013-02-06 19:43:37 +0200

    Launching enhancement

--- src/luna_navigator.cpp
+++ src/luna_navigator.cpp
@@ -19,13 +19,14 @@ bool getPath(nav_msgs::GetPlan::Request &req, nav_msgs::GetPlan::Response &res)
 	if (mapClient.call(mapService)) {
 		ROS_INFO("Got map from service (%d nodes)", mapService.response.map.data.size());
 		ROS_INFO("------------------------------------");
+		cout << mapService.response.map.data;
 		for (unsigned int i = 0; i < mapService.response.map.info.height; i++) {
 		    stringstream sstr;
 			for (unsigned int j = 0; j < mapService.response.map.info.width; j++) {
-				int8_t probability = mapService.response.map.data.at(i*mapService.response.map.info.width+j);
-				sstr << probability << " ";
+				//int8_t probability = mapService.response.map.data.at(i*mapService.response.map.info.width+j);
+				cout << setw(3) << mapService.response.map.data.at(i*mapService.response.map.info.width+j);
 			}
-			ROS_INFO("%s", sstr.str().c_str());
+			cout << endl;
 		}
 		ROS_INFO("\n");
 		
