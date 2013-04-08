37fb806a9de4d2e80626a49fc6393ae85c84f9e9
ivany4 <ivany4@me.com>
2013-04-08 20:29:28 +0300

    Setup enhancement

--- setup.bash
+++ setup.bash
@@ -16,3 +16,7 @@ lunagazebo() {
 lunactrl() {
 	rosrun teleop_base teleop_base_keyboard base_controller/command:=$1
 }
+
+export GAZEBO_PLUGIN_PATH=~/local/lib/gazebo_plugins:$GAZEBO_PLUGIN_PATH
+export GAZEBO_MODEL_PATH=`rospack find lunabotics`/gazebo/models:$GAZEBO_MODEL_PATH
+export GAZEBO_MODEL_DATABASE_URI=""
